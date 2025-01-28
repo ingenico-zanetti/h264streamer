d7c4c2778d050f0dfdfc59035fee7cb6fabdd8c7
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-01-28 23:11:29 +0100

    First working version: - parse the h264 input - accept incoming TCP connection (hardcoded on port 56789) - forward data to incoming connection, start at GOP beginning

--- h264streamer.c
+++ h264streamer.c
@@ -8,7 +8,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <linux/limits.h>
+#include <sys/time.h>
 
+int listenSocket(struct in_addr *address, unsigned short port){
+	struct sockaddr_in local_sock_addr = {.sin_family = AF_INET, .sin_addr = *address, .sin_port = port};
+	int listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// fprintf(stderr, "listen_socket=%d" "\n", listen_socket);
+	const int enable = 1;
+	if (setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0){
+		// fprintf(stderr,"setsockopt(SO_REUSEADDR) failed" "\n");
+	}else{
+		int error = bind(listen_socket, (struct sockaddr *)&local_sock_addr, sizeof(local_sock_addr));
+		if(error){
+			close(listen_socket);
+			listen_socket = -1;
+		}
+	}
+	if(listen_socket >= 0){
+		int error = listen(listen_socket, 1);
+		if(error){
+			close(listen_socket);
+			listen_socket = -2;
+		}
+	}
+	return listen_socket;
+}
 
 #define BUFFER_SIZE (1 * 1000 * 1000)
 #define MAX_OUTPUTS (16)
@@ -41,6 +75,17 @@ static void contextInitialize(parserContext_s *context){
 	context->outputBufferIndex = 0;
 }
 
+static int contextFirstSlotAvailable(parserContext_s *context){
+	int i = 0;
+	while(i < MAX_OUTPUTS){
+		if(-1 == context->outputs[i].fd){
+			return(i);
+		}
+		i++;
+	}
+	return(-1);
+}
+
 static void analyze_and_forward(parserContext_s *context, const uint8_t *buffer, ssize_t length){
 	ssize_t i = length;
 	const uint8_t *p = buffer;
@@ -53,7 +98,7 @@ static void analyze_and_forward(parserContext_s *context, const uint8_t *buffer,
 		}else if((context->index == 3) && (1 == octet)){
 			context->index++;
 		}else if(context->index == 4){
-			printf("0x00000001%02X found" "\n", octet);
+			// printf("0x00000001%02X found" "\n", octet);
 			context->index = 0;
 			if(0x67 == octet){
 				newGOP = 1;
@@ -83,7 +128,7 @@ static void analyze_and_forward(parserContext_s *context, const uint8_t *buffer,
 				}
 				// Move current "tag" to start of buffer
 				// by moving nothing
-				printf("flushed %d, reset index to 5" "\n", lengthToFlush);
+				// printf("flushed %d, reset index to 5" "\n", lengthToFlush);
 				context->outputBufferIndex = 5;
 				context->outputBuffer[4] = octet;
 			}else{
@@ -97,14 +142,62 @@ int main(int argc, const char *argv[]){
 	int in  = STDIN_FILENO;
 	uint8_t *buffer = (uint8_t *)malloc(BUFFER_SIZE);
 	if(buffer != NULL){
+		struct in_addr listenAddress = {0}; // bind to this address for score connections
+		int listeningSocket = listenSocket(&listenAddress, htons(56789));
 		parserContext_s context;
 		contextInitialize(&context);
 		for(;;){
-			ssize_t lus = read(in, buffer, BUFFER_SIZE);
-			if(lus <= 0){
-				break;
+			void updateMax(int *m, int n){
+				int max = *m;
+				if(n > max){
+					*m = n;
+				}
 			}
-			analyze_and_forward(&context, buffer, lus);
+			int max = -1;
+			fd_set fds;
+			FD_ZERO(&fds);
+			FD_SET(STDIN_FILENO, &fds); updateMax(&max, STDIN_FILENO);
+			if(-1 != listeningSocket && (0 <= contextFirstSlotAvailable(&context))){
+				FD_SET(listeningSocket, &fds); updateMax(&max, listeningSocket);
+			}
+			int i = MAX_OUTPUTS;
+			while(i--){
+				int fd = context.outputs[i].fd;
+				if(-1 != fd){
+					FD_SET(fd, &fds); updateMax(&max, fd);
+				}
+			}
+			struct timeval timeout = {.tv_sec = 1, .tv_usec = 0};
+			int selected = select(max + 1, &fds, NULL, NULL, &timeout);
+			if(selected > 0){
+				// Check forwarding socket for error (read ready should not happen)
+				i = MAX_OUTPUTS;
+				while(i--){
+					int fd = context.outputs[i].fd;
+					if(FD_ISSET(fd, &fds)){
+						close(fd);
+						context.outputs[i].fd = -1;
+						context.outputs[i].state = OUTPUT_STATE_IDLE;
+						printf("slot %d add an error, closing fd %d" "\n", i, fd);
+					}
+				}
+				// Check listening socket for incoming connection
+				if(FD_ISSET(listeningSocket, &fds)){
+					int index  = contextFirstSlotAvailable(&context);
+					context.outputs[index].state = OUTPUT_STATE_IDLE;
+					context.outputs[index].fd = accept(listeningSocket, NULL, NULL);
+					printf("accepted connexion to slot %d)" "\n", index);
+				}
+				if(FD_ISSET(STDIN_FILENO, &fds)){
+					// printf("data available on stdin" "\n");
+					ssize_t lus = read(in, buffer, BUFFER_SIZE);
+					if(lus <= 0){
+						break;
+					}
+					analyze_and_forward(&context, buffer, lus);
+				}
+				fflush(stdout);
+			}
 		}
 		free(buffer);
 	}
